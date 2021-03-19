5e84183c65ca4dbd0eb7bec0867ee4df9c69f149
Kauê Cavalcante <61801350+kauecdev@users.noreply.github.com>
2021-03-19 16:04:44 -0300

    Added React Project

---
+++ helloworld-react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import reactIcon from './assets/react-icon.png';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={reactIcon} alt="React" />
+
+      <h1>Hello world in React!</h1>
+      <h3>By: Kauê Cavalcante</h3>
+    </div>
+  );
+}
+
+export default App;
